c15f37a7506001ad2c85fbad414fd2269ded7b03
Jake Howard <git@theorangeone.net>
2015-08-20 14:25:30 +0100

    NL

--- build-js.sh
+++ build-js.sh
@@ -4,4 +4,5 @@ cp -r src/* build/
 rm -rf build/injections/*
 mkdir -p build/src/injections
 for f in src/injections/*; do uglifyjs $f --compress --mangle -o build/$f; done
-cp build/src/injections/* build/injections/ && rm -rf build/src
+cp build/src/injections/* build/injections/
+rm -rf build/src
