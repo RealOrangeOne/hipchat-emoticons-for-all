e65dba16b9b2191bd40e3d1de160187ce12a917c
Jake Howard <git@theorangeone.net>
2015-08-20 11:17:37 +0100

    Moved logic to use new file structure

--- firefox/index.js
+++ firefox/index.js
@@ -1,9 +1,10 @@
 var self = require('sdk/self');
 var pageMod = require("sdk/page-mod");
-var sites = require("data/site-decoder")
+var sites = require("data/site-decoder.json")
 
-
-pageMod.PageMod({
-  include: "*.facebook.com",
-  contentScriptFile: ["./jquery.js", "./script.js"]
-});
+for (var i = 0; i < sites.length; i++) {
+    pageMod.PageMod({
+      include: sites[i][0],
+      contentScriptFile: ["lib/jquery.js", "./image_lookup.js", "injections/" + sites[i][1]]
+    });
+}
--- src/site-decoder.js
+++
@@ -1,4 +0,0 @@
-var site_decoder = { // Site Identifier : Injection JS
-    "*.facebook.com":"facebook.js"
-}
-module.exports = site_decoder;
