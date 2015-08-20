e1d030db0e25cee0f82430540a12a28055f3c103
Jake Howard <git@theorangeone.net>
2015-08-20 14:22:56 +0100

    Moved hacky script out to seperate file

---
+++ build-js.sh
@@ -0,0 +1,7 @@
+#~!/usr/bin/bash
+
+cp -r src/* build/
+rm -rf build/injections/*
+mkdir -p build/src/injections
+for f in src/injections/*; do uglifyjs $f --compress --mangle -o build/$f; done
+cp build/src/injections/* build/injections/ && rm -rf build/src
