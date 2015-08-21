b34242049beb265676a2a83beee7f23d6b8b8a25
Jake Howard <git@theorangeone.net>
2015-08-21 09:13:38 +0100

    Removed debugging

--- firefox/index.js
+++ firefox/index.js
@@ -6,10 +6,6 @@ for (var i = 0; i < sites.length; i++) {
     pattern = Object.keys(sites[i])[0];
     script = sites[i][pattern];
     content_scripts = ["./lib/jquery.js", "./image-lookup.js", "./injections/" + script];
-    console.log('script ' + script);
-    console.log('pattern ' + pattern);
-    console.log('Content Scripts: ' + content_scripts);
-
     pageMod.PageMod({
       include: pattern,
       contentScriptFile: content_scripts
