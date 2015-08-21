b3b169f0f3f4fe5b83710639219b4b650ae43ba5
Jake Howard <git@theorangeone.net>
2015-08-21 10:29:01 +0100

    Added testing record

--- src/image-lookup.js
+++ src/image-lookup.js
@@ -1,4 +1,4 @@
-var image_decoder = { // Image tag (no brackets) : Image URL
-    "noot" : "https://dujrsrsgsd3nh.cloudfront.net/img/emoticons/23362/noot-1438871175.png"
+var image_decoder = {
+    "as" : "https://dujrsrsgsd3nh.cloudfront.net/img/emoticons/23362/noot-1438871175.png"
 
 }
