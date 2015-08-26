e23b1361de1a5c9e7a8cd142eb89ff2178163008
Jake Howard <git@theorangeone.net>
2015-08-26 09:58:22 +0100

    NL

--- src/injections/trello.js
+++ src/injections/trello.js
@@ -2,6 +2,7 @@ function change_card_title() {
     inject_image('.list-card-title');
     inject_image('.window-title-text');
 }
+
 function change_list_title() {
     inject_image('.list-header-name');
     inject_image('.window-header-inline-content');
@@ -10,38 +11,47 @@ function change_list_title() {
 function change_comment() {
     inject_image('.action-comment');
 }
+
 $('.list-header-name').on('change load', function() {
     change_list_title();
 });
+
 $('.active-card').on('click', function() {
     change_list_title();
     change_card_title();
     change_comment();
 });
+
 $('.card-detail-edit textarea').on('change input', function() {
     markdown_injector('.card-detail-edit textarea');
 });
+
 $('.phenom-action').on('DOMNodeInserted DOMNodeRemoved change load', function() {
     change_card_title();
     change_list_title();
     change_comment();
 });
+
 $('.js-add-card').on('click', function() {
     change_card_title();
 });
+
 $('.action-card').on('click', function() {
     change_card_title();
     change_list_title();
     change_comment();
 });
+
 $('.action-comment').on('click', function() {
     change_comment();
 });
+
 $('.js-save-edit').on('click', function() {
     change_card_title();
     change_list_title();
     change_comment();
 });
+
 $(document).on('ready', function() {
     change_comment();
     change_card_title();
