9565a167458f649e5d98f0e3ddc12445ab832e65
Jake Howard <git@theorangeone.net>
2015-09-22 16:09:24 +0100

    Added PR commit support

--- src/injections/github.js
+++ src/injections/github.js
@@ -12,7 +12,9 @@ function change_wiki() {
 }
 
 function change_commits() {
-    insert_emoticons('table.files a .message')
+    insert_emoticons('table.files a .message');
+    insert_emoticons('table.timeline-commits commit-message a.message');
+
 }
 
 $('.js-comment-container').on('load change', change_comments);
@@ -26,6 +28,7 @@ $('form .timeline-comment textarea[placeholder="Leave a comment"]').on('change i
 $('#markdown-body').on('DOMNodeInserted DOMNodeRemoved change load', change_wiki);
 
 $('table.files').on('DOMNodeInserted DOMNodeRemoved change load', change_commits);
+$('table.timeline-commits').on('DOMNodeInserted DOMNodeRemoved change load', change_commits);
 
 $(document).on('ready', function() {
   console.log("Ready");
