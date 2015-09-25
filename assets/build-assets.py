67c2dba88b100fbfe0b8c25f76384469f0d984f0
Jake Howard <git@theorangeone.net>
2015-09-25 09:41:27 +0100

    Revert "Dynamically get urls from hipchat. No need for repo-based images!"
    This reverts commit 6cde8ec7852982bad94b4ab104522a2776eef95b.

--- assets/build-assets.py
+++ assets/build-assets.py
@@ -1,11 +1,8 @@
-import json, requests
+import json
 from glob import glob
-from lxml import html
-from collections import namedtuple
 
-CUSTOM_ASSETS_URL="https://raw.githubusercontent.com/RealOrangeOne/hipchat-emoticons-for-all/master/assets/"
+ASSETS_URL="https://raw.githubusercontent.com/RealOrangeOne/hipchat-emoticons-for-all/master/assets/"
 
-Emoticon = namedtuple('Emoticon', ['ident', 'url'])
 
 def get_icon_name(path):
     return get_filename(path).replace('.png', '').replace('.gif', '')
@@ -14,23 +11,11 @@ def get_filename(path):
     return path.replace('assets/', '')
 
 
-page = requests.get("https://www.hipchat.com/emoticons")
-html_tree = html.fromstring(page.text)
-emoticon_paths = html_tree.xpath("//div[@class='emoticon-block']/img/@src")
-emoticon_names = html_tree.xpath("//div[@class='emoticon-block']/div/text()")
-
-
-emoticons = []
-for i in range(len(emoticon_names)-1):
-    emoticons.append(Emoticon(emoticon_names[i][1:-1], emoticon_paths[i]))
-
-images = []
-for emoticon in emoticons:
-    images.append({emoticon.ident: emoticon.url})
-
 files = glob('assets/*.png') + glob('assets/*.gif')
+images = []
+
 for filename in files:
-    images.append({get_icon_name(filename): CUSTOM_ASSETS_URL + get_filename(filename)})
+    images.append({get_icon_name(filename): ASSETS_URL + get_filename(filename)})
 
 
 image_decoder = {"images":images}
