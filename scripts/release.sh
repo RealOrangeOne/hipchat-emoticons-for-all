662c3b92ab014b7e2a6d68ae2f9c50df35f72756
Jake Howard <git@theorangeone.net>
2015-10-05 15:52:45 +0100

    Added release zipping for chrome

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,3 +9,8 @@ cd -
 
 echo "Building Chrome Extension..."
 crx pack chrome -o 'chrome/hipchat-emoticons-for-all.crx' -p 'chrome/key.pem'
+
+echo "Zipping Chrome Extension..."
+cd chrome
+zip 'release.zip' -1 -9 -T 'hipchat-emoticons-for-all.crx' 'manifest.json' 'data/*'
+cd -
